14962ec7614066604a87696abf88786e25a0d1e8
Luan Bianchini <luanbianchine1@gmail.com>
2023-02-09 19:43:16 -0300

    style: reduzindo o numero de tabs no codigo

--- src/components/cards/cardView.js
+++ src/components/cards/cardView.js
@@ -1,39 +1,33 @@
 import { Text, View, Button } from "react-native";
 import { useState } from "react";
 
-export default function CardView({name, description, resposta, refe}) {
-    const [show, setShow] = useState();
+export default function CardView({ name, description, resposta, refe }) {
+  const [show, setShow] = useState();
 
-    const press = () => {
-        setShow(true)
-    }
+  const press = () => {
+    setShow(true);
+  };
 
-    const pressed = () => {
-        setShow(false)
-    }
+  const pressed = () => {
+    setShow(false);
+  };
 
-    return (
-        <View>
-            {!show && (
-                <>
-                    <Text>{name}</Text>
-                    <Text>{description}</Text>
-                    <Button 
-                        title='saiba mais'
-                        onPress={() => press()}
-                    />
-                </>
-            )}
-            {show && (
-                <>
-                    <Text>{resposta}</Text>
-                    <Text>{refe}</Text>
-                    <Button 
-                        title='voltar'
-                        onPress={() => pressed()}
-                    />
-                </>
-            )}
-        </View>
-    )
+  return (
+    <View>
+      {!show && (
+        <>
+          <Text>{name}</Text>
+          <Text>{description}</Text>
+          <Button title="saiba mais" onPress={() => press()} />
+        </>
+      )}
+      {show && (
+        <>
+          <Text>{resposta}</Text>
+          <Text>{refe}</Text>
+          <Button title="voltar" onPress={() => pressed()} />
+        </>
+      )}
+    </View>
+  );
 }
--- src/components/podcast/podcast.cardView.js
+++ src/components/podcast/podcast.cardView.js
@@ -1,71 +1,71 @@
 import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Audio } from 'expo-av';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Audio } from "expo-av";
 
-export default function PodCardView({name, refe}) {
-    const [play, setplay] = useState()
-    const [sound, setSound] = useState(null);
+export default function PodCardView({ name, refe }) {
+  const [play, setplay] = useState();
+  const [sound, setSound] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            const soundObject = new Audio.Sound();
-            try {
-                await soundObject.loadAsync(require('../../../public/sound/Yamete.mp3'));
-                setSound(soundObject);
-            } catch (error) {
-                console.log(error);
-            }
-        })();
-    },[]);
+  useEffect(() => {
+    (async () => {
+      const soundObject = new Audio.Sound();
+      try {
+        await soundObject.loadAsync(require("../../../public/sound/Yamete.mp3"));
+        setSound(soundObject);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
-    const playSound = async () => {
-        try {
-            await sound.playAsync()
-            setplay(true)
-        } catch (error) {
-            console.log(error)
-        }
+  const playSound = async () => {
+    try {
+      await sound.playAsync();
+      setplay(true);
+    } catch (error) {
+      console.log(error);
     }
-    
-    const pause = async () => {
-        try {
-            await sound.pauseAsync()
-            setplay(false)
-        } catch (error) {
-            console.log(error)
-        }
+  };
+
+  const pause = async () => {
+    try {
+      await sound.pauseAsync();
+      setplay(false);
+    } catch (error) {
+      console.log(error);
     }
-    
-    const replay = async () => {
-        try {
-            await sound.stopAsync();
-            await sound.unloadAsync();
-            const newSound = new Audio.Sound();
-            await newSound.loadAsync(require('../../../public/sound/Yamete.mp3'));
-            setSound(newSound);
-        } catch (error) {
-            console.log(error);
-        }
+  };
+
+  const replay = async () => {
+    try {
+      await sound.stopAsync();
+      await sound.unloadAsync();
+      const newSound = new Audio.Sound();
+      await newSound.loadAsync(require("../../../public/sound/Yamete.mp3"));
+      setSound(newSound);
+    } catch (error) {
+      console.log(error);
     }
-    
-    return(
-        <View>
-            <Text>{name}</Text>
-            <Text>{refe}</Text>
-            {!play && (
-                <TouchableOpacity onPress={playSound}>
-                    <Ionicons name="play" size={20} />
-                </TouchableOpacity>
-            )}
-            {play && (
-                <TouchableOpacity onPress={pause}>
-                    <Ionicons name="pause" size={20} />
-                </TouchableOpacity>
-            )}
-            <TouchableOpacity onPress={replay}>
-                <Ionicons name="sync" size={20} />
-            </TouchableOpacity>
-        </View>
-    )
+  };
+
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text>{refe}</Text>
+      {!play && (
+        <TouchableOpacity onPress={playSound}>
+          <Ionicons name="play" size={20} />
+        </TouchableOpacity>
+      )}
+      {play && (
+        <TouchableOpacity onPress={pause}>
+          <Ionicons name="pause" size={20} />
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity onPress={replay}>
+        <Ionicons name="sync" size={20} />
+      </TouchableOpacity>
+    </View>
+  );
 }

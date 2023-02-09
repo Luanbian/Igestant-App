1eb9cb086b3a3997e635aba93144748a76e1d3a3
Luan Bianchini <luanbianchine1@gmail.com>
2023-02-08 21:04:03 -0300

    feat: add header

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import Routes from './src/routes'
+import MyHeader from './src/components/header/header';
 
 export default function App() {
   return (
-    <Routes/>
+    <>
+      <MyHeader/>
+      <Routes/>
+    </>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const MyHeader = () => (
+  <View style={styles.headerContainer}>
+    <Text style={styles.headerText}>My Header</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 60,
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default MyHeader;

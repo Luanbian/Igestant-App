5bad2f2b4f91c45d2d43335b203ca9c264b1e852
Luan Bianchini <luanbianchine1@gmail.com>
2023-02-19 18:01:21 -0300

    feat: o bruto do estilo principal está feito

--- src/components/principal/principal.cardView.js
+++ src/components/principal/principal.cardView.js
@@ -1,5 +1,6 @@
-import { Text, View, Button, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { useState } from "react";
+import { style } from "../../style/principal/principal.style";
 
 export default function PrincipalCardView({ name, description, resposta, refe, image }) {
   const [show, setShow] = useState();
@@ -13,22 +14,35 @@ export default function PrincipalCardView({ name, description, resposta, refe, i
   };
 
   return (
-    <View>
+    <>
       {!show && (
-        <>
-          <Text>{name}</Text>
-          <Text>{description}</Text>
-          <Image source={image} style={{width: 108, height: 105}}/>
-          <Button title="saiba mais" onPress={() => press()} />
-        </>
+        <View style={style.card}>
+          <Text style={style.title}>{name}</Text>
+          <View style={style.posiDescImg}>
+            <Text style={style.description}>{description}</Text>
+            <Image source={image} style={style.image}/>
+          </View>
+          <View style={style.posiButton}>
+            <TouchableOpacity onPress={() => press()} style={style.button}>
+              <Text style={style.label}>Saiba mais</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       )}
       {show && (
         <>
-          <Text>{resposta}</Text>
-          <Text>{refe}</Text>
-          <Button title="voltar" onPress={() => pressed()} />
+          <Text style={style.title}>{name}</Text>
+          <View style={style.posiResRefe}>
+            <Text style={style.resposta}>{resposta}</Text>
+            <Text style={style.refe}>{refe}</Text>
+          </View>
+          <View style={style.posiButton}>
+            <TouchableOpacity onPress={() => pressed()} style={style.button}>
+              <Text style={style.label}>Voltar</Text>
+            </TouchableOpacity>
+          </View>
         </>
       )}
-    </View>
+    </>
   );
 }
---
+++ src/style/principal/principal.style.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  card: {
+    marginBottom: '8%'
+  },
+  title: {
+    fontFamily: 'PoppinsMedium',
+    fontSize: 18,
+    marginLeft: '2%'
+  },
+  description: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 16,
+    width: "67%",
+    marginTop: '3%'
+  },
+  image: {
+    width: 108,
+    height: 105,
+    borderRadius: 15
+  },
+  posiDescImg: {
+    flexDirection: 'row',
+    marginBottom: '2%',
+    marginLeft: '2%'
+  },
+  button: {
+    backgroundColor: '#B6DBF2',
+    borderRadius: 20,
+    width: 108
+  },
+  posiButton: {
+    alignItems: 'flex-end',
+    marginEnd: '5%'
+  },  
+  label: {
+    textAlign: 'center',
+    fontFamily: 'PoppinsRegular',
+    fontSize: 13
+  },
+  resposta: {
+    fontFamily: 'PoppinsLight',
+    fontSize: 13,
+    marginBottom: '2%'
+  },
+  refe: {
+    fontFamily: 'PoppinsExtraLight',
+    fontSize: 10
+  },
+  posiResRefe: {
+    marginBottom: '2%',
+    marginLeft: '2%',
+    marginEnd: '3%'
+  }
+});

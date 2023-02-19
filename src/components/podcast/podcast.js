a71ae7478a54352b10cdab88eed5c986c89f03c6
Luan Bianchini <luanbianchine1@gmail.com>
2023-02-19 16:03:41 -0300

    feat: barra de pesquisa

--- src/components/podcast/podcast.js
+++ src/components/podcast/podcast.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
-import datasPodcast from './info.podcast';
-import PodCardView from './podcast.cardView';
+import React, {useState} from "react";
+import { View, FlatList, TextInput } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import datasPodcast from "./info.podcast";
+import PodCardView from "./podcast.cardView";
 
 export default function PodcastCard() {
+  const [filteredData, setFilteredData] = useState(datasPodcast);
+  const [search, setSearch] = useState("");
+
+  function searchFilter(text) {
+    if (text) {
+      const newData = datasPodcast.filter((item) => {
+        const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setSearch(text);
+    } else {
+      setFilteredData(datasPodcast);
+      setSearch(text);
+    }
+  }
   return (
     <View>
+      <TextInput placeholder="pesquise" value={search} onChangeText={(text) => searchFilter(text)} />
+      <Ionicons name="search" />
       <FlatList
-        data={datasPodcast}
-        renderItem={({item}) => 
-          <PodCardView
-            name={item.name} 
-            refe={item.refe}
-            audio={item.audio}
-          />
-        }
-        keyExtractor={item => item.id}
+        data={ filteredData || datasPodcast}
+        renderItem={({ item }) => (
+          <PodCardView name={item.name} refe={item.refe} audio={item.audio} />
+        )}
+        keyExtractor={(item) => item.id}
       />
     </View>
-  )
+  );
 }
--- src/components/principal/principal.js
+++ src/components/principal/principal.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
-import datasPrincipal from './info.principal';
-import PrincipalCardView from './principal.cardView';
+import React, {useState} from "react";
+import { View, FlatList, TextInput } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import datasPrincipal from "./info.principal";
+import PrincipalCardView from "./principal.cardView";
 
 export default function PrincipalCard() {
+  const [filteredData, setFilteredData] = useState(datasPrincipal);
+  const [search, setSearch] = useState("");
+
+  function searchFilter(text) {
+    if (text) {
+      const newData = datasPrincipal.filter((item) => {
+        const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setSearch(text);
+    } else {
+      setFilteredData(datasPrincipal);
+      setSearch(text);
+    }
+  }
+
   return (
     <View>
+      <TextInput placeholder="pesquise" value={search} onChangeText={(text) => searchFilter(text)} />
+      <Ionicons name="search" />
       <FlatList
-        data={datasPrincipal}
-        renderItem={({item}) => 
-          <PrincipalCardView 
-            name={item.name} 
+        data={ filteredData || datasPrincipal}
+        renderItem={({ item }) => (
+          <PrincipalCardView
+            name={item.name}
             description={item.description}
             resposta={item.resposta}
             refe={item.refe}
           />
-        }
-        keyExtractor={item => item.id}
+        )}
+        keyExtractor={(item) => item.id}
       />
     </View>
-  )
+  );
 }

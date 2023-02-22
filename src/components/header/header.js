886aa914fe53cd1a311627127dade2837881399c
Luan Bianchini <luanbianchine1@gmail.com>
2023-02-21 23:45:02 -0300

    style: bruto do header feito

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,14 +8,18 @@ const MyHeader = () => (
 
 const styles = StyleSheet.create({
   headerContainer: {
-    height: 60,
-    backgroundColor: '#f2f2f2',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 45,
+    marginTop: '10%',
+    marginBottom: '2%',
+    backgroundColor: 'rgba(255, 95, 180, 0.74)',
+    display: 'flex',
+    justifyContent: 'center'
   },
   headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 30,
+    marginLeft: '2%',
+    fontFamily: 'DancingScript',
+    color: '#FAFAFA'
   },
 });
 
